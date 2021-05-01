27e00522c54ba8793be8cf71271077e982c4c89a
Oscar Koeroo <okoeroo@gmail.com>
2021-05-01 22:30:51 +0200

    updated the release creation

--- notes/create_release.sh
+++ notes/create_release.sh
@@ -8,9 +8,17 @@ if [ "$ANSWER" != 'y' ]; then
 fi
 rm -rv dist/
 
+read -p 'What is the new version?: ' VERSION
+echo "Is this correct [y/N]: $VERSION"
+
+if [ "$ANSWER" != 'y' ]; then
+    exit 1
+fi
+
+
 python3 -m build || exit 1
 python3 -m twine upload --repository testpypi dist/* --verbose || exit 1
 
 echo "Wait for processing..."
-echo python3 -m pip install -i https://test.pypi.org/simple/ pyDANETLSA==0.0.1
+echo python3 -m pip install -i https://test.pypi.org/simple/ pyDANETLSA==${ANSWER}
 
