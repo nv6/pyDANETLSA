eb42e3416e13f4721eca7fe4d22349bd1e86febb
Oscar Koeroo <okoeroo@gmail.com>
2025-01-31 23:54:00 +0100

    release fix

---
+++ notes/create_testing_release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+pwd
+
+
+read -p 'Did you update the setup.cfg file? [y/N]: ' ANSWER
+if [ "$ANSWER" != 'y' ]; then
+    exit 1
+fi
+echo
+
+read -p 'Remove dist/ [y/N]: ' ANSWER
+if [ "$ANSWER" != 'y' ]; then
+    exit 1
+fi
+rm -rv dist/
+
+VERSION=$(sed -n 's/^version *= *//p' setup.cfg)
+read -p "Detected version in setup.cfg is \"$VERSION\". Is this correct? " ANSWER
+if [ "$ANSWER" != 'y' ]; then
+    exit 1
+fi
+
+
+echo "Build..."
+python3 -m build || exit 1
+
+echo
+echo "Twine..."
+python3 -m twine upload --repository testpypi dist/* --verbose || exit 1
+echo
+
+echo "test install with:"
+echo "
+python3 -m venv .venv
+. .venv/bin/activate
+python3 -m pip install -i https://test.pypi.org/simple/ pyDANETLSA==${ANSWER}
+"
