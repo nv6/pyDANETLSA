c789eb64a0497b11e169735ed31b7d93d260c2bb
Oscar Koeroo <okoeroo@gmail.com>
2025-02-01 21:31:58 +0100

    fixes in builds

--- notes/create_production_release.sh
+++ notes/create_production_release.sh
@@ -3,7 +3,7 @@
 pwd
 
 
-read -p 'Did you update the setup.cfg file? [y/N]: ' ANSWER
+read -p 'Did you update the pyproject.toml file? [y/N]: ' ANSWER
 if [ "$ANSWER" != 'y' ]; then
     exit 1
 fi
@@ -15,7 +15,7 @@ if [ "$ANSWER" != 'y' ]; then
 fi
 rm -rv dist/
 
-VERSION=$(sed -n 's/^version *= *//p' setup.cfg)
+VERSION=$(sed -n 's/^version = "\([^"]*\)"/\1/p' pyproject.toml)
 read -p "Detected version in setup.cfg is \"$VERSION\". Is this correct? " ANSWER
 if [ "$ANSWER" != 'y' ]; then
     exit 1
@@ -36,6 +36,6 @@ mkdir /tmp/testingground
 cd /tmp/testingground
 python3 -m venv .venv
 . .venv/bin/activate
-python3 -m pip install -r requirements.txt
-python3 -m pip install -i https://pypi.org/simple/ pyDANETLSA==${ANSWER}
+python3 -m pip install cryptography pyOpenSSL requests dnspython
+python3 -m pip install -i https://pypi.org/simple/ pyDANETLSA==${VERSION}
 "
