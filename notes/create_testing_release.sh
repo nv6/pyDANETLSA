49caad720b0a856459eac870354502576c1f976e
Oscar Koeroo <okoeroo@gmail.com>
2025-02-01 23:46:36 +0100

    fixing testing build script

--- notes/create_testing_release.sh
+++ notes/create_testing_release.sh
@@ -23,22 +23,29 @@ echo "---"
 
 
 read -p "Upload to testpypi? [y/N] " ANSWER
-if [ "$ANSWER" != 'y' ]; then
-    exit 1
+if [ "$ANSWER" = 'y' ]; then
+    echo
+    echo "Twine..."
+    python3 -m twine upload --repository testpypi dist/* --verbose || exit 1
 fi
 
-echo
-echo "Twine..."
-python3 -m twine upload --repository testpypi dist/* --verbose || exit 1
 
 echo
 
 echo "test install with:"
 echo "
+
+cd /tmp
+rm -r /tmp/testingground
 mkdir /tmp/testingground
+
+cp `pwd`/test/m.py /tmp/testingground
+
 cd /tmp/testingground
 python3 -m venv .venv
 . .venv/bin/activate
 python3 -m pip install cryptography pyOpenSSL requests dnspython
 python3 -m pip install -i https://test.pypi.org/simple/ pyDANETLSA==${VERSION}
+
+python3 m.py
 "
